a7699b7195d4f90a81144bbbd9fedc41cb0d04fe
ZivCarmi <carmi.ziv@gmail.com>
2024-01-30 17:40:29 +0200

    delete test

--- src/components/Head.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Head() {
-  return <h1 className="game-title">Candy Crush Clone</h1>;
-}
--- src/components/Score.js
+++
@@ -1,7 +0,0 @@
-import { useScore } from "./ScoreProvider";
-
-export default function Score() {
-  const { score } = useScore();
-
-  return <div className="score">{score}</div>;
-}
